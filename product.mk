8cfca31d2363edc78555a11d5f6700da8b429cbc
Ash <androidash@proton.me>
2025-02-01 13:58:04 +0530

    extra: Update lawnchair for Project Zenith
    Signed-off-by: Amrito | アンブロシア <bhabanidas431@gmail.com>

--- product.mk
+++ product.mk
@@ -1,8 +1,4 @@
-# TARGET_LAUNCHER options:
-# 1 - stock (default)
-# 2 - lawnchair
-# 3 - pixel (valid only on gapps builds)
-ifeq ($(TARGET_LAUNCHER),2)
+ifeq ($(TARGET_INCLUDE_LAWNCHAIR),true)
   PRODUCT_PACKAGES += \
     Lawnchair \
     LawnchairOverlay \
